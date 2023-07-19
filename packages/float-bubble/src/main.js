0ac2f59614396a2f8946281d46f88dff34aafaff
viarotel <viarotel@qq.com>
2023-07-19 14:25:35 +0800

    update

--- packages/float-bubble/src/main.js
+++ packages/float-bubble/src/main.js
@@ -1,6 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import plugins from './plugins/index'
 import libs from './libs/index'
 
 import 'virtual:uno.css'
@@ -8,7 +7,6 @@ import './styles/index'
 
 const app = createApp(App)
 
-app.use(plugins)
 app.use(libs)
 
 app.mount('#app')
