839f8ceb9e2443a17c300d20a3c07f359feb30f8
viarotel <viarotel@163.com>
2023-07-24 17:45:44 +0800

    feat: add eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,18 +1,3 @@
 module.exports = {
-  extends: "@antfu",
-  rules: {
-    // js
-    "curly": "off",
-    "n/prefer-global/process": "off",
-    "unused-imports/no-unused-vars": "off",
-    "no-console": "off",
-    "semi": "error",
-    // ts
-    "@typescript-eslint/ban-ts-comment": "off",
-    "@typescript-eslint/quotes": "off",
-    "@typescript-eslint/semi": "off",
-    // vue
-    "vue/no-unused-refs": "off",
-    "vue/component-tags-order": "off",
-  },
+  extends: "@viarotel-org/eslint-config",
 }
---
+++ packages/eslint-config/src/index.js
@@ -0,0 +1,18 @@
+export default {
+  extends: "@antfu",
+  rules: {
+    // js
+    "curly": "off",
+    "n/prefer-global/process": "off",
+    "unused-imports/no-unused-vars": "off",
+    "no-console": "off",
+    "semi": "error",
+    // ts
+    "@typescript-eslint/ban-ts-comment": "off",
+    "@typescript-eslint/quotes": "off",
+    "@typescript-eslint/semi": "off",
+    // vue
+    "vue/no-unused-refs": "off",
+    "vue/component-tags-order": "off",
+  },
+}
